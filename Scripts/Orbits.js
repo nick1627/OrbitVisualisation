4dbb9ac050dc464d8ece9b33d00cc6c5557bf096
nick1627 <nick.rayns1627@gmail.com>
2019-08-27 17:14:16 +0100

    basic orbit functionality

--- Scripts/Orbits.js
+++ Scripts/Orbits.js
@@ -7,21 +7,108 @@ class Orbit{
         this.ArgOfPe = ArgOfPe;
         this.a = SemimajorAxis;
         this.e = Eccentricity;
+
+        //this.PlotData = this.GetPlotData();
+    }
+
+    GetPlotData(OrbitPath){
+        let x = OrbitPath[0];
+        let y = OrbitPath[1];
+        let z = OrbitPath[2];
+
+        let OrbitData = [({
+            type: "scatter3d",
+            mode: "lines",
+            name: "Orbit",
+            x: x,
+            y: y,
+            z: z,
+
+            line: {
+                width: 6,
+                color: this.Colour,
+                //reversescale: false
+            }
+        })];
+
+        return OrbitData;
     }
 
     GetPath(){
-        //gets orbit path on 2d plane
+        //gets orbit path
+        let a = this.a;
+        let e = this.e;
+        //first create x values.
+        let n = 1000;
+        let x = [];
+        let y = [];
+        let z = [];
+        for (let i = 0; i <= n; i++){
+            x.push(-a + i*(2*a/n));
+            y.push(Math.sqrt((a**2 - x[i]**2)*(1 - e**2)));
+            z.push(0);
+        }
+        for (let i = 0; i <= n; i++){
+            x.push(a - i*(2*a/n));
+            y.push(-Math.sqrt((a**2 - x[i]**2)*(1 - e**2)));
+            z.push(0);
+        }
+        for (let i = 0; i < x.length; i++){
+            x[i] = x[i] - a*e;
+        }
+
+        //transform
+        return [x, y, z];
     }
 
-    Transform(){
+    Transform(x, y, z){
         //transforms orbit points into actual 3d position
     }
 
-    PlotOrbit(GraphName){
-        //get orbit data
-        //plot orbit data
-
+    NewPlot(GraphName, GraphData, AxisLimit){
+        Plotly.purge(GraphName);
+        Plotly.newPlot(GraphName, GraphData, setLayout('x', 'y', 'z', AxisLimit));
     }
+
+    UpdatePlot(GraphName, GraphData, AxisLimit){
+        Plotly.react(GraphName, GraphData, setLayout('x', 'y', 'z', AxisLimit));
+    }
+}
+
+
+function setLayout(sometitlex, sometitley, sometitlez, AxisLimit){
+    //set layout of graphs. 
+    
+    let new_layout = {//layout of 3D graph
+        //showlegend: false,
+        //showscale: false,
+        uirevision: 'dataset',
+        margin: {
+            l: 1, r: 1, b: 10, t: 1, pad: 0
+        },
+        dragmode: 'turntable',
+        scene: {
+            aspectmode: "cube",
+            // xaxis: {range: [-0.05, 0.05], title: sometitlex},//, showticklabels: false},
+            // yaxis: {range: [-0.01, 0.01], title: sometitley},//, showticklabels: false},
+            // zaxis: {range: [-0.01, 0.01], title: sometitlez},//, showticklabels: false},
+            xaxis: {range: [-AxisLimit, AxisLimit], title: sometitlex},//, showticklabels: false},
+            yaxis: {range: [-AxisLimit, AxisLimit], title: sometitley},//, showticklabels: false},
+            zaxis: {range: [-AxisLimit, AxisLimit], title: sometitlez},//, showticklabels: false},
+            
+            //aspectmode: "manual",
+            aspectratio: {
+                x: 5, y: 1, z: 1,
+            },
+
+            camera: {
+                up: {x: 0, y: 0, z: 1},//sets which way is up
+                eye: {x: 1, y: 1, z: 1}//adjust camera starting view
+            }
+        },
+    };
+
+    return new_layout;
 }
 
 function GetNewInputs(){
@@ -30,6 +117,7 @@ function GetNewInputs(){
     let omega = document.getElementById("ArgOfPeSlider").value;
 
     let a = document.getElementById("aSlider").value;
+    a = a*10**4;
     let e = document.getElementById("eSlider").value;
    
 
@@ -40,7 +128,8 @@ function GetNewInputs(){
 }
 
 function Main(PlotNew = false){
-    NewVariables = GetNewInputs();
+    let AxisLimit = 2*10**5;
+    let NewVariables = GetNewInputs();
     let Omega = NewVariables[0];
     let i = NewVariables[1];
     let omega = NewVariables[2];
@@ -50,13 +139,13 @@ function Main(PlotNew = false){
     let PlaySpeed = NewVariables[6];
 
     let OrbitA = new Orbit(1, Omega, i, omega, a, e);
-    OrbitA.GetPath();
-    OrbitA.GetPlotData();
+    let OrbitPath = OrbitA.GetPath();
+    let PlotData = OrbitA.GetPlotData(OrbitPath);
 
     if (PlotNew){
-        OrbitA.NewPlot("3DGraph");
+        OrbitA.NewPlot("3DGraph", PlotData, AxisLimit);
     }else{
-        OrbitA.UpdatePlot("3DGraph");
+        OrbitA.UpdatePlot("3DGraph", PlotData, AxisLimit);
     }
 }
 
